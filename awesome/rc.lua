091d1519aef1e7ecae7129bec27c30e741dcafe8
Bjørn Gilstad <bjorn.gilstad@gmail.com>
2013-06-13 00:47:34 +0200

    Awesome: Changed default terminal
    Changed default terminl to rxvt-unicode

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -40,7 +40,7 @@ end
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "rxvt-unicode"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
